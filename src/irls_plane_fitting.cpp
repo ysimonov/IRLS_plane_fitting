df134e68da903b50927815f161b99b3c009ba587
Yevgeniy <simonov.yevgeniy@gmail.com>
2022-10-31 19:24:26 +0800

    feat: calculating eigenvalues and eigenvectors of the covariance matrix

--- src/irls_plane_fitting.cpp
+++ src/irls_plane_fitting.cpp
@@ -30,15 +30,26 @@ void PlaneFittingIRLS::fitPlane()
 {
     // get points
     Eigen::MatrixX3f points_xyz = this->getPoints();
+    size_t number_of_points = this->getNumberOfPoints();
 
-    // calculate centroid
-    Eigen::Vector3f centroid = points_xyz.colwise().mean();
+    // compute centroid vector
+    Eigen::RowVector3f centroid = points_xyz.colwise().mean();
 
-    std::cout << "Centroid: " << centroid << std::endl;
+    std::cout << "Centroid: " << std::endl << centroid << std::endl;
 
-    // calculate covariance matrix
-    Eigen::MatrixXf centered = points_xyz.rowwise() - points_xyz.colwise().mean();
-    Eigen::MatrixXf covariance_matrix = (centered.adjoint() * centered) / static_cast<double>(getNumberOfPoints() - 1);
+    // compute covariance matrix
+    Eigen::MatrixX3f centered = points_xyz.rowwise() - points_xyz.colwise().mean();
+    Eigen::Matrix3f covariance_matrix = (centered.adjoint() * centered) / static_cast<double>(getNumberOfPoints() - 1);
 
-    std::cout << "Covariance Matrix: " << covariance_matrix << std::endl;
+    std::cout << "Covariance Matrix: " << std::endl << covariance_matrix << std::endl;
+
+    // compute eigenvalues and eigenvectors of the covariance matrix
+    // since covariance matrix is symmetric, it can only have real eigenvalues
+    Eigen::EigenSolver<Eigen::Matrix3f> eigensolver;
+    eigensolver.compute(covariance_matrix, /* computeEigenvectors */ true);
+    Eigen::Vector3f eigenvalues = eigensolver.eigenvalues().real();
+    Eigen::Matrix3f eigenvectors = eigensolver.eigenvectors().real();
+
+    std::cout << "Eigenvalues: " << std::endl << eigenvalues << std::endl;
+    std::cout << "Eigenvectors: " << std::endl << eigenvectors << std::endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
 
-    Eigen::MatrixX3f points_xyz{{1, 3, 7}, {9, 5, 8}, {4, 10, 15}};
+    Eigen::MatrixX3f points_xyz{{1, 3, 7}, {9, 5, 8}, {4, 10, 15}, {1.5, 3.3, 4.5}, {1.2, 10.5, 7.7}};
     PlaneFittingIRLS plane_fit(points_xyz);
     plane_fit.fitPlane();
 
